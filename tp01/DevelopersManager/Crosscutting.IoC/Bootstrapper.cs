a98cc1fde25f2439e451749ab4b6c59a3ca6dbc4
franciscocamellon <franciscocamellon@gmail.com>
2021-08-16 22:31:30 -0300

    Update

--- tp01/DevelopersManager/Crosscutting.IoC/Bootstrapper.cs
+++ tp01/DevelopersManager/Crosscutting.IoC/Bootstrapper.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Domain.Model.Interfaces.
 
-namespace Domain.Service.Services
+namespace Crosscutting.IoC
 {
-    public class DeveloperService : IDeveloperService
+    public class Bootstrapper
     {
     }
 }
