d40d2e3f104cccee08d049d7c27826521b2c4ca8
franciscocamellon <franciscocamellon@gmail.com>
2021-08-18 16:51:18 -0300

    Add developer controller

---
+++ tp01/DevNetwork/Presentation/Data/MobileAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Domain.Model.Model;
+
+namespace Presentation.Data
+{
+    public class MobileAppDbContext : DbContext
+    {
+        public MobileAppDbContext (DbContextOptions<MobileAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Domain.Model.Model.DeveloperModel> Developers { get; set; }
+    }
+}
